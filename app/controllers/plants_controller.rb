c8e3a9fad5f81d68c756774c47675824b17de359
Valentin Lassartesse <valentin.lassartesse@gmail.com>
2020-01-08 21:23:48 +0100

    crud fait

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,21 +1,46 @@
 class PlantsController < ApplicationController
+  before_action :set_plant, only: [:edit, :update, :destroy]
   def index
     @plants = Plant.all
   end
 
   def new
+    @plant = Plant.new
   end
 
   def create
+    @plant = Plant.new(plants_params)
+    if @plant.save
+      redirect_to plants_path
+    else
+      render :new
+    end
   end
 
   def edit
-    @plant = Plant.find(params[:id])
   end
 
   def update
+    if @plant.update(plants_params)
+      redirect_to plants_path
+    else
+      render :edit
+    end
   end
 
-  def delete
+  def destroy
+    @plant.destroy
+
+    redirect_to plants_path
+  end
+
+  private
+
+  def plants_params
+    params.require(:plant).permit(:name, :description)
+  end
+
+  def set_plant
+    @plant = Plant.find(params[:id])
   end
 end
